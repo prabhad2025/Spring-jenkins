81dc4398c7b72e5500faf51fdbd84b698c9263ee
prabhad2025 <prabhakaran.d94@gmail.com>
2025-04-10 13:23:42 +0530

    intial commit

---
+++ src/main/java/com/techno/Spring_jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.techno.Spring_jenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application Executed...");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
